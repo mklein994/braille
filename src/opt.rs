fc9dd4c60d5d8f3f8b60061dda425d8ed12af1fb
Matthew Klein <matthew.klein@birchwood.ca>
2023-11-14 10:55:19 -0600

    Print usage message with error context

--- src/opt.rs
+++ src/opt.rs
@@ -1,3 +1,5 @@
+use anyhow::Context;
+
 #[derive(Debug)]
 pub struct Opt {
     pub minimum: f64,
@@ -7,26 +9,38 @@ pub struct Opt {
 
 impl Opt {
     pub fn from_args(args: Vec<String>) -> anyhow::Result<Self> {
+        Self::try_from_args(args).context("Usage: <STDIN> | braille <minimum> <maximum> [<width>]")
+    }
+
+    fn try_from_args(args: Vec<String>) -> anyhow::Result<Self> {
+        anyhow::ensure!(
+            args.len() == 2 || args.len() == 3,
+            "Invalid number of arguments"
+        );
+
         let mut args = args.into_iter();
 
-        let minimum = args.next().and_then(|x| x.parse().ok()).ok_or_else(|| {
-            anyhow::anyhow!("Usage: <STDIN> | braille <minimum> <maximum> [<width>]")
-        })?;
+        let minimum = args
+            .next()
+            .unwrap()
+            .parse()
+            .context("invalid minimum value")?;
         let maximum = args
             .next()
-            .map(|x| x.parse::<f64>())
-            .transpose()?
-            .ok_or_else(|| anyhow::anyhow!("missing maximum argument"))?;
+            .unwrap()
+            .parse()
+            .context("invalid maximum value")?;
         let width = args
             .next()
             .map(|x| x.parse())
-            .transpose()?
+            .transpose()
+            .context("invalid width value")?
             .or_else(|| {
                 terminal_size::terminal_size().map(|(terminal_size::Width(width), _)| width)
             })
             .unwrap_or(80);
 
-        assert!(minimum < maximum);
+        anyhow::ensure!(minimum < maximum);
 
         Ok(Self {
             minimum,
